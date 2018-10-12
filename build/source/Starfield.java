9cc858e0196a3ac8362996d9cd31f19ee4122e28
Jaekim06789 <Jaekim06789@gmail.com>
2018-10-12 10:45:14 -0700

    a first

--- build/source/Starfield.java
+++ build/source/Starfield.java
@@ -1,15 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 //your code here
 NormalParticle [] field;
-void setup()
+public void setup()
 {
 	//your code here
-	size(500,500);
+	
 	field = new NormalParticle[130];
 	for(int i = 0; i < field.length; i++){
 		field[i] = new NormalParticle();
 	}
 }
-void draw()
+public void draw()
 {
 	//your code here
 	background(0);
@@ -23,12 +39,12 @@ class NormalParticle
 	//your code here
 	double myX, myY, myAngle, mySpeed, speedX, speedY;
 	int radX, radY;
-	void move(){
+	public void move(){
 		if(myX <= 0 || myX > 500){
 			myX = width/2;
 			myY = height/2;
 			myAngle = (Math.random()*(2*Math.PI));
-			mySpeed = ((Math.random()*8.25)-5);
+			mySpeed = ((Math.random()*8.25f)-5);
 			speedX = Math.cos(myAngle)*mySpeed;
 			speedY = Math.sin(myAngle)*mySpeed;
 		}
@@ -36,14 +52,14 @@ class NormalParticle
 			myX = width/2;
 			myY = height/2;
 			myAngle = (Math.random()*(2*Math.PI));
-			mySpeed = ((Math.random()*8.25)-2);
+			mySpeed = ((Math.random()*8.25f)-2);
 			speedX = Math.cos(myAngle)*mySpeed;
 			speedY = Math.sin(myAngle)*mySpeed;
 		}
 		myX += speedX;
 		myY += speedY;
 	}
-	void show(){
+	public void show(){
 		ellipse((float)myX, (float)myY,radX,radY);
 	}
 	public NormalParticle(){
@@ -52,7 +68,7 @@ class NormalParticle
 		myX = width/2;
 		myY = height/2;
 		myAngle = (Math.random()*(2*Math.PI));
-		mySpeed = ((Math.random()*8.25));
+		mySpeed = ((Math.random()*8.25f));
 		speedX = Math.cos(myAngle)*mySpeed;
 		speedY = Math.sin(myAngle)*mySpeed;
 	}
@@ -78,4 +94,14 @@ class OddballParticle //uses an interface
 class JumboParticle //uses inheritance
 {
 	//your code here
+}
+  public void settings() { 	size(500,500); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
